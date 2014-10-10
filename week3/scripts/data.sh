cb5889b99d7ab405411e7c5756af872933198d46
RhysC92 <rhys_coleman92@hotmail>
2014-10-10 15:32:05 +0100

    Data.sh runable

---
+++ week3/scripts/data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Please enter your name " Name
+read -p "What month were you born " Month
+read -p "Your favourite color " Color
+
+echo "Hello $Name you were born in $Month and your favourite color is $Colour "
